de5f7e59a926261b9677dfc371a71b71de14a837
Andre Velasquez <Andre Velasquez>
2017-09-05 14:14:39 -0600

    Terminando BinTree

--- ProyectoEstructuras/src/proyectoestructuras/BinTree.java
+++ ProyectoEstructuras/src/proyectoestructuras/BinTree.java
@@ -10,31 +10,82 @@ package proyectoestructuras;
  * @author andre
  */
 public class BinTree {
-    BinTree LNode, RNode;
-    char info;
+    BinTree LNode = null, RNode = null;
+    String info;
     
-    BinTree(BinTree LNode, BinTree RNode, char info){
+    BinTree(BinTree LNode, BinTree RNode, String info){
         this.LNode = LNode;
         this.RNode = RNode;
+        this.info = info;
+    }
+    
+    BinTree(String info){
         this.info = info;
     }
     
     BinTree(){
-        
+        this.LNode = null;
+        this.RNode = null;
     }
     
-    public void insertLNode(BinTree LNode, char info){
+    public void insertLNode(BinTree LNode, String info){
         this.LNode = LNode;
         this.LNode.setInfo(info);
     }
     
-    public void insertRNode(BinTree RNode, char info){
+    public void insertRNode(BinTree RNode, String info){
         this.RNode = RNode;
         this.RNode.setInfo(info);
     }
     
-    public void setInfo(char info){
+    public void setInfo(String info){
         this.info = info;
+    }
+    
+    public String getInfo(){
+        return this.info;
+    }
+    
+    public BinTree getLNode(){
+        return this.LNode;
+    }
+    
+    public BinTree getRNode(){
+        return this.RNode;
+    }
+    
+    
+    
+    public int evaluar(BinTree root){
+        if(root == null){
+            return 0;
+        }
+        
+        if(root.getLNode() == null && root.getRNode() == null){
+            //System.out.println("info " + root.getInfo());
+            return Integer.parseInt(root.getInfo());
+        }
+        
+        int valor_l = evaluar(root.getLNode()); //evaluar left node
+        int valor_r = evaluar(root.getRNode()); // evaluar right node
+
+        if(root.getInfo().equals("+")){
+            return valor_l + valor_r;
+        }
+        
+        else if(root.getInfo().equals("-")){
+            return valor_l - valor_r;
+        }
+        
+        else if(root.getInfo().equals("*")){
+            return valor_l * valor_r;
+        }
+        
+        else if (root.getInfo().equals("^")){
+            return (int) Math.pow(valor_l, valor_r);
+        }
+        
+        else return valor_l / valor_r;
     }
     
 }
