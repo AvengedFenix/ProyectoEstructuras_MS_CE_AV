769ef42b49d929af1e91c417cf35268050f4f0eb
Andre Velasquez <Andre Velasquez>
2017-09-06 16:07:51 -0600

    fae

--- ProyectoEstructuras/src/proyectoestructuras/TreeNode.java
+++ ProyectoEstructuras/src/proyectoestructuras/TreeNode.java
@@ -16,12 +16,8 @@ public class TreeNode {
     private int order;
     private int evaluacion ;
     private int hash;
-<<<<<<< HEAD
-
-=======
     boolean visited = false;
     
->>>>>>> 96197c048dd986fdb6604bfb9fb1b2b4ce1614b0
     private ArrayList<TreeNode> children = new ArrayList();
     private TreeNode parent = null;
 
@@ -173,16 +169,30 @@ public class TreeNode {
             
             return padre;
         } else {
-            for (int i = 0; i < padre.getChildren().size()-1; i++) {
+            for (int i = 0; i < padre.getChildren().size()/*-1*/; i++) {
                 if (padre.getChildren().get(i).getVisited() == true) {
                     System.out.println("Already visited");
                 }else{
                     return Lectura(padre.getChildren().get(i));
                 }
+                
+                if(padre.allChildrenVisited()){
+                    padre.Evaluar();
+                }
             }
         }
+        
+        return null;
     }
 
+    public void recorridoPostorden(TreeNode nodo){
+        if(nodo != null){
+            for(int i = 0; i < nodo.getChildren().size(); i++){
+                recorridoPostorden(nodo.getChildren().get(i));
+            }
+            System.out.print(nodo.getEvaluacion() + " ");
+        }
+    }
     
  public boolean getVisited(){
         return this.visited;
@@ -200,16 +210,24 @@ public class TreeNode {
         this.evaluacion = x;
     }
     
-    public void Evaluar(TreeNode nodo){
+    public void Evaluar(){
         int x = 0;
-        if(!nodo.getChildren().isEmpty()){
+        if(!this.getChildren().isEmpty()){
             for (int i = 0; i < this.getChildren().size(); i++) {
                 x += this.getChildren().get(i).getEvaluacion();
             }
-        nodo.setEvaluacion(x);
+        this.setEvaluacion(x);
         }
         
         
+    }
+    
+    public boolean allChildrenVisited(){
+        for (int i = 0; i < this.getChildren().size(); i++) {
+            if(!this.getChildren().get(i).getVisited()) return false;
+        }
+        
+        return true;
     }
     
 }
